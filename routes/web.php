d47c775acc0a84396b9fe2ff0adecab6eeca9c45
Valentina-Fr <v_frasca@virgilio.it>
2021-09-30 13:49:05 +0200

    Print world dynamically

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = [
+        "name"  => "World"
+    ];
+    return view('home', $data);
 });
